54450d8808f7702f029da2c3c58871d8e96a6454
Mukhammad Rizki <rizkyRE7@gmail.com>
2023-11-09 22:02:47 +0700

    Fixed Shop's Path

--- shop/urls.py
+++ shop/urls.py
@@ -4,6 +4,6 @@ from shop import views
 urlpatterns = [
     path('shops/', views.all_shop, name='all-shops'),
     path('shops/analytics/', views.shop_analytics, name='shop-analytics'),
-    path('shops/products/', views.shop_products, name='shop-products'),
+    path('shops/shop-products/', views.shop_products, name='shop-products'),
     path('shops/add-shop/', views.add_shop, name='add-shop')
 ]
