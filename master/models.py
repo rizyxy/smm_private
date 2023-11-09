b77be8c846ff0af1dd7e917e9897ce0e829a4e90
Mukhammad Rizki <rizkyRE7@gmail.com>
2023-11-09 18:43:26 +0700

    Revised Models

--- master/models.py
+++ master/models.py
@@ -1,33 +1,33 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=255)
 
 class Shop(models.Model):
-    name = models.CharField()
-    location = models.CharField()
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
 
 class ShopHistory(models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
-    rating = models.DecimalField()
+    rating = models.FloatField()
 
 class Product(models.Model):
     owner = models.ForeignKey(Shop, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     url = models.TextField()
-    name = models.CharField()
+    name = models.CharField(max_length=255)
     description = models.TextField()
-    condition = models.CharField()
+    condition = models.CharField(max_length=255)
 
 class ProductHistory(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    price = models.DecimalField()
+    price = models.FloatField()
     sold = models.IntegerField()
-    rating = models.DecimalField()
+    rating = models.FloatField()
     stock = models.IntegerField()
 
 class Event(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=255)
     startdate = models.DateTimeField()
     enddate = models.DateTimeField()
 
