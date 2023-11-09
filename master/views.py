f5e5bf63438d06b7ff948f57c06c6bc3cae1453b
Mukhammad Rizki <rizkyRE7@gmail.com>
2023-11-09 18:46:42 +0700

    Added Master Templates

--- master/views.py
+++ master/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def dashboard(request):
+    template = loader.get_template('dashboard.html')
+    return HttpResponse(template.render(None, request))
