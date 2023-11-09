c596761400080a7fbc26e3cf9ad38e0076419d06
Mukhammad Rizki <rizkyRE7@gmail.com>
2023-11-09 18:29:34 +0700

    Registered Model to Admin

--- master/admin.py
+++ master/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from master.models import *
 
-# Register your models here.
+admin.site.register([Category, Shop, ShopHistory, Product, ProductHistory, Event, EventParticipant])
